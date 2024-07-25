b7b08631349fcf0e815cca49dd38e7f22c45eff6
darealyeeto <157327107+darealyeeto@users.noreply.github.com>
2024-07-25 17:19:58 +0100

    Update TypeShit.js

--- TypeShit.js
+++ TypeShit.js
@@ -2,5 +2,5 @@ ts
 import { Messages } from "enmity/metro/common";
 
 Patcher.before(Messages, "sendMessage", (_, [, msg]) => {
-  msg.content += "\nEnd of Message"
+  msg.content += "\ntype shit"
 });
