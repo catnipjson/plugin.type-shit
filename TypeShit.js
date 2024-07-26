e6cfdbf8f906cb50f564a507c47f1e9d98fc99af
darealyeeto <157327107+darealyeeto@users.noreply.github.com>
2024-07-26 18:23:09 +0100

    Update TypeShit.js

--- TypeShit.js
+++ TypeShit.js
@@ -1,6 +1,25 @@
-ts
+import { Patcher } from "enmity";
 import { Messages } from "enmity/metro/common";
 
-Patcher.before(Messages, "sendMessage", (_, [, msg]) => {
-  msg.content += "\ntype shit"
-});
+// Define the plugin object
+const myPlugin = {
+  // Plugin name
+  name: "MessageAppender",
+  
+  // Plugin initialization method
+  onStart() {
+    // Patching the sendMessage method to append text
+    Patcher.before(Messages, "sendMessage", (_, [, msg]) => {
+      msg.content += "\nEnd of Message";
+    });
+  },
+
+  // Plugin cleanup method
+  onStop() {
+    // Unpatch all patches done by this plugin
+    Patcher.unpatchAll();
+  }
+};
+
+// Export the plugin for the modding framework to load
+export default myPlugin;
